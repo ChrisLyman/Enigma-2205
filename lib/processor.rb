010c06573ee1ed95ab7852332e400f45697c9feb
Chris Lyman <christopher.lyman0@gmail.com>
2022-06-10 22:13:18 -0400

    refactor assign key methods to processor.rb

--- lib/processor.rb
+++ lib/processor.rb
@@ -1,27 +1,28 @@
 class Processor
 
+  attr_reader :random_code
+
+  def initialize
+    @random_code = generate_random_code
+  end
+
   def generate_random_code
     rand(99999).to_s.rjust(5, "0")
   end
 
   def assign_key_a
-    random_code = generate_random_code
-    key_a = random_code[0...-3]
+    key_a = @random_code[0...-3]
   end
 
   def assign_key_b
-    random_code = generate_random_code
-    key_b = random_code[1...-2]
+    key_b = @random_code[1...-2]
   end
 
   def assign_key_c
-    random_code = generate_random_code
-    key_c = random_code[2...-1]
+    key_c = @random_code[2...-1]
   end
 
   def assign_key_d
-    random_code = generate_random_code
-    key_d = random_code[3..4]
+    key_d = @random_code[3..4]
   end
-
 end
