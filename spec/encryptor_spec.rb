40204e0bd10465a398794774c1642516279c8eaf
Chris Lyman <christopher.lyman0@gmail.com>
2022-06-13 16:50:07 -0400

    refactor call method for encryptor.rb

--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -1,14 +1,20 @@
 require './lib/encryptor'
 
 RSpec.describe Encryptor do
+  describe '#initialize' do
+    it 'exists' do
+      encryptor = Encryptor.new
+
+      expect(encryptor). to be_instance_of(Encryptor)
+    end
+  end
+
   describe '#call' do
-    it '' do
+    it 'will encrypt a message' do
       encryptor = Encryptor.new
       processor = Processor.new
 
-      expect(encryptor.call("hello world", "02715", "040895")).to eq({encryption: "keder ohulw",
-                                                                                         key: "02715",
-                                                                                        date: "040895"})
+      expect(encryptor.call("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
     end
   end
 end
